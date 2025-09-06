4d1cb45b0a8932f60c2674d1eaa5be7d32b1b14c
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-09-06 14:03:57 +0600

    adding new arrow function example and theroy

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,2 +1,11 @@
-const addtwonumber=(a,b) => a+b
-console.log(addtwonumber(10,12))
+// // old 
+
+// function add (a,b ){
+//     return a+b;
+// }
+// console.log(add(3,4));
+
+// but in the arrow function we can write this code very easyly
+
+const add=(a,b) => a+b;
+console.log(add(3,4))
