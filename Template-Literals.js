6df62c88d25415b3d6b7d07a666c8fdc0a202b63
Hasib Al Mamun <hasib.gub221@gmail.com>
2025-09-06 15:35:21 +0600

    adding template literals

---
+++ Template-Literals.js
@@ -0,0 +1,23 @@
+const name = 'Hasib';
+const msg =`Hello,
+
+
+${name} Welcome to ES6`;
+console.log(msg);
+
+
+// 2nd 
+
+const poem =`Rose are red,
+sky is blue ,
+Field are green,
+water has no colour`
+
+console.log(poem)
+
+
+// String interpolation
+
+let a = 10;
+let b = 7;
+console.log(`The sum of ${a} and the sum of ${b} is : ${a+b}`);
